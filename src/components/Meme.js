b5de4b4da8e16630443f061f5505df1236bbf81f
jrfm <moesch.jonas@protonmail.com>
2023-03-10 09:41:12 +0100

    add image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,6 +2,9 @@ import React from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+  console.log(memeImage);
+
   /**
    * returns random number between min and max
    * The maximum is exclusive and the minimum is inclusive
@@ -16,35 +19,40 @@ export default function Meme() {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  function handleClick() {
+  function getMemeImage() {
+    // get array with url's
     const memesArray = memesData.data.memes;
     const ranNum = getRandomInt(0, 100);
     const url = memesArray[ranNum].url;
-    console.log(url);
-    console.log("Click");
+
+    setMemeImage(url);
+    console.log(memeImage);
   }
   return (
-    <div className="form">
-      <div className="input--container">
-        <input
-          type="text"
-          className="form--input"
-          placeholder="top text"
-        ></input>
-        <input
-          type="text"
-          className="form--input"
-          placeholder="bottom text"
-        ></input>
+    <div className="main">
+      <div className="form">
+        <div className="input--container">
+          <input
+            type="text"
+            className="form--input"
+            placeholder="top text"
+          ></input>
+          <input
+            type="text"
+            className="form--input"
+            placeholder="bottom text"
+          ></input>
+        </div>
+        <button className="form--btn" onClick={getMemeImage}>
+          <p className="form--btn--text">Get new Meme image</p>
+          <img
+            className="form--btn--image"
+            src="/images/picture.png"
+            alt=""
+          ></img>
+        </button>
       </div>
-      <button className="form--btn" onClick={handleClick}>
-        <p className="form--btn--text">Get new Meme image</p>
-        <img
-          className="form--btn--image"
-          src="/images/picture.png"
-          alt=""
-        ></img>
-      </button>
+      <img className="meme--image" src={memeImage} alt=""></img>
     </div>
   );
 }
